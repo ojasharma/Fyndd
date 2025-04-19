930d86b1ab8129f3f9d406a2f6e30b567c95133e
ojassharma <ojasvisharmacs@gmail.com>
2025-04-19 22:14:38 +0530

    added favicon and init navbar

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,10 @@
+// src/app/components/Navbar.tsx
+export default function Navbar() {
+  return (
+    <nav className="w-full bg-white px-4 py-3 shadow-sm">
+      <div className="flex items-center justify-start">
+        <img src="/Fyndd.png" alt="fyndd logo" className="h-8 w-auto pl-4" />
+      </div>
+    </nav>
+  );
+}
